d07402c8bfc668d65caf27fe5dbe73f6da06f1e9
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-02 12:55:19 +0300

    gets a summary of array elements total using reduce method

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+//creates a variable that store the value executed by the reducer
+//acc is the accumulator value, curr is the current value
+//0 is the starting point/initial value
+const totalBatteries = batteryBatches.reduce((acc, curr)=>{
+  return acc + curr
+})
+console.log(totalBatteries)
+
+/*alt solution
+function reducer (acc, curr){
+  return acc + curr
+}
+const totalBatteries = batteryBatches.reduce(reducer )
+console.log(totalBatteries) */
